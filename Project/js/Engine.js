8f5a8c98b28bcb7b39db842f31fa86e29e5247b8
nature-modren <121174751+nature-modren@users.noreply.github.com>
2023-01-02 11:59:53 +0700

    Create Engine.js
    Membuat js

---
+++ Project/js/Engine.js
@@ -0,0 +1,14 @@
+function  tampilan(){
+    
+var begitu=document.getElementById("form"). select1.value;
+var p_tampilan=document.getElementById("container")
+
+if (begitu =='yoi'){
+    p_tampilan.innerHTML="hi";
+}else if(begitu =='apa'){
+    p_tampilan.innerHTML="siapa kamu?";
+}else{
+    p_tampilan.innerHTML="hehehheheh";
+}
+}
+
