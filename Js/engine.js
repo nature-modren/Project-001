5b15c8166ff3a05548ec41d53c71bc840b6af6a4
nature-modren <121174751+nature-modren@users.noreply.github.com>
2023-01-15 12:42:09 +0700

    Update engine.js
    Ubah kata sandi

--- Js/engine.js
+++ Js/engine.js
@@ -3,10 +3,10 @@ var user = document.getElementById("form").user.value;
 var pass = document.getElementById("form").pass.value;
 var p_container=document.getElementById("container")
 
-if(user== "Amri" && pass== "kakashi") {
+if(user== "Mika" && pass== "mikachii") {
     location.href="uji tester web 3.html"
 }
-else if (user=="mika" && pass== "mikachii") {
+else if (user=="putra" && pass== "mikachii") {
     alert("welcome putra")
 }
 else {
