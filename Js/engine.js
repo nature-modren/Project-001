1c134d3302299890d0b90c6d02a2e8d7f458d73a
Amri <121174751+nature-modren@users.noreply.github.com>
2023-07-24 10:07:31 +0700

    Update engine.js <add>password debug

--- Js/engine.js
+++ Js/engine.js
@@ -14,3 +14,14 @@ else {
 }
 }
 
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById('passwordInput');
+  
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+  } else {
+    passwordInput.type = 'password';
+  }
+}
+
+
